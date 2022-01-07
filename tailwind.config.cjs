8e9ff87230db1d721e1341677499a6952c2e397a
Benjamin Keating <ben@spreadsheetsusa.com>
2022-01-07 11:36:28 -0600

    Adds TailwindCSS to project

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.svelte'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
